397245058a391263332c66bc37265364c6482f2d
JustADot14 <77913694+JustADot14@users.noreply.github.com>
2021-07-04 00:50:39 +0630

    Update config.php

--- utilities/config.php
+++ utilities/config.php
@@ -6,10 +6,10 @@
 //$pass = "";
 //$dbname = "manga_proj";
 
- $dbhost = "us-cdbr-east-02.cleardb.com";
- $user = "be9d3bc6ef6d71";
- $pass = "bb43538f";
- $dbname = "heroku_8306e03e292f018";
+ $dbhost = "us-cdbr-east-04.cleardb.com";
+ $user = "b315cc8efc76f9";
+ $pass = "60732d24";
+ $dbname = "heroku_f9dfc34899b2484";
 
 $conn = mysqli_connect($dbhost, $user, $pass);
 mysqli_select_db($conn, $dbname);
